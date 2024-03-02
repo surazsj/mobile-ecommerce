ffb1a5a2e1d1db51d0b39412e83e5e650e74aec3
surazsj <59992730+surazsj@users.noreply.github.com>
2024-03-02 10:14:32 +0545

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from 'expo-font';
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 export default function App() {
+
+  const [fontsLoaded]= useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+  })
+
+  const onLayoutRootView = useCallback(async() => {
+
+    if (fontsLoaded){
+      await SplashScreen.hideAsync();
+      
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+    
+  }
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style= {styles.textStyle}> working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +46,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontSize: 20,
+    fontFamily: "bold"
+  }
 });
